016c6fb0912024ab42d02f96e18c1b8e9fd57cb7
Kian <kchawks39@gmail.com>
2025-07-10 11:32:41 +0100

    Update validators.py

--- utils/validators.py
+++ utils/validators.py
@@ -74,20 +74,20 @@ def check_clothing_duplicates(items: list[Clothing]):
     return errors
 
 
-def bad_char(obj, id_attr: str) -> str:
-    """ The characters ',% can't be in any product variables. Check if they have any and return where.
-        Input for id_attr should be the code/name of item preferred when returning an error message.
-    """
-    bad_fields = []
-    for field, value in vars(obj).items():     # Grab each variable and the value for the product
-            if isinstance(value, str):          # Avoid type error
-                if any(char in value for char in BAD_CHARS):    # Check if any bad chars are in the value
-                    bad_fields.append(field)
+# def bad_char(obj, id_attr: str) -> str:
+#     """ The characters ',% can't be in any product variables. Check if they have any and return where.
+#         Input for id_attr should be the code/name of item preferred when returning an error message.
+#     """
+#     bad_fields = []
+#     for field, value in vars(obj).items():     # Grab each variable and the value for the product
+#             if isinstance(value, str):          # Avoid type error
+#                 if any(char in value for char in BAD_CHARS):    # Check if any bad chars are in the value
+#                     bad_fields.append(field)
 
-    if bad_fields:
-            line = getattr(obj, "excel_line", None)
-            id = getattr(obj, id_attr, None)
-            return f"Line {line} \u00A0\u00A0|\u00A0\u00A0 {id} contains invalid character(s) {BAD_CHARS}"
+#     if bad_fields:
+#             line = getattr(obj, "excel_line", None)
+#             id = getattr(obj, id_attr, None)
+#             return f"Line {line} \u00A0\u00A0|\u00A0\u00A0 {id} contains invalid character(s) {BAD_CHARS}"
 
 
 
